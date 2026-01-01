ea2c70367d0ab65fcaac3aed30366ddf7a6749ae
komil14 <abdulvokhidovkomiljon@gmail.com>
2026-01-01 17:45:13 +0900

    feat: add ThemeProvider, Logo, DropdownMenu, ModeToggle, and Sheet components

---
+++ src/libs/components/ui/mode-toggle.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "./button";
+
+export function ModeToggle() {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  }, [resolvedTheme, setTheme]);
+
+  return (
+    <Button
+      variant="ghost"
+      className="group/toggle h-10 w-10 !px-0 text-foreground"
+      onClick={toggleTheme}
+    >
+      <SunIcon className="hidden [html.dark_&]:block" />
+      <MoonIcon className="hidden [html.light_&]:block" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
